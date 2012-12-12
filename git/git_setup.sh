612f7ab0d93cdb907d162740ff7787bdf5649f64
zsxwing <zsxwing@gmail.com>
2012-12-12 15:55:11 +0800

    Add the git setup script

---
+++ git/git_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# set user information
+git config --global user.name zsxwing
+git config --global user.email zsxwing@gmail.com
+
+# set alias
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.last 'log -l HEAD'
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+
+git config --global color.ui true
+
